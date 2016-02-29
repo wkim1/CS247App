7cebe2cec07bb5cffae2c863973c92751ca5031c
Will Kim <wkim1@stanford.edu>
2016-02-28 22:33:38 -0800

    exercise list added

--- CS247App/ViewController.swift
+++ CS247App/ViewController.swift
@@ -14,11 +14,37 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
-        let chooseExerciseButton   = UIButton(frame: CGRectMake(100, 100, 100, 50))
+        let exerciseListImage: UIImage = UIImage(named: "ExercisesList")!
+        let exerciseListImageView = UIImageView(image: exerciseListImage)
+        
+        // Set dimensions to full size of iPhone 6 Plus screen.
+        exerciseListImageView.frame = CGRectMake(0, 0, 414, 736)
+        self.view.addSubview(exerciseListImageView)
+        
+        
+        let chooseExerciseButton   = UIButton(frame: CGRectMake(280, 130, 100, 50))
         chooseExerciseButton.backgroundColor = UIColor.grayColor()
-        chooseExerciseButton.setTitle("Go to exercise", forState: UIControlState.Normal)
+        chooseExerciseButton.setTitle("Do this", forState: UIControlState.Normal)
         chooseExerciseButton.addTarget(self, action: "toExercise:", forControlEvents: .TouchUpInside)
         self.view.addSubview(chooseExerciseButton)
+        
+        let chooseExerciseButton2   = UIButton(frame: CGRectMake(280, 210, 100, 50))
+        chooseExerciseButton2.backgroundColor = UIColor.grayColor()
+        chooseExerciseButton2.setTitle("Do this", forState: UIControlState.Normal)
+        chooseExerciseButton2.addTarget(self, action: "toExercise:", forControlEvents: .TouchUpInside)
+        self.view.addSubview(chooseExerciseButton2)
+        
+        let chooseExerciseButton3   = UIButton(frame: CGRectMake(280, 290, 100, 50))
+        chooseExerciseButton3.backgroundColor = UIColor.grayColor()
+        chooseExerciseButton3.setTitle("Do this", forState: UIControlState.Normal)
+        chooseExerciseButton3.addTarget(self, action: "toExercise:", forControlEvents: .TouchUpInside)
+        self.view.addSubview(chooseExerciseButton3)
+        
+        let chooseExerciseButton4   = UIButton(frame: CGRectMake(280, 370, 100, 50))
+        chooseExerciseButton4.backgroundColor = UIColor.grayColor()
+        chooseExerciseButton4.setTitle("Do this", forState: UIControlState.Normal)
+        chooseExerciseButton4.addTarget(self, action: "toExercise:", forControlEvents: .TouchUpInside)
+        self.view.addSubview(chooseExerciseButton4)
     }
 
     override func didReceiveMemoryWarning() {
