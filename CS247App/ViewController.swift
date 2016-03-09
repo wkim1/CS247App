0fb2f98c010d6b70213155d9cf56f9fb3f4ba95f
Vivek Choksi <viv.choksi@gmail.com>
2016-03-09 00:42:00 -0800

    Instantiate first few views in IB

--- CS247App/ViewController.swift
+++ CS247App/ViewController.swift
@@ -19,11 +19,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    @IBAction func toExercise(sender: AnyObject) {
-        performSegueWithIdentifier("toExercise", sender: sender)
-    }
-
 
 }
 
