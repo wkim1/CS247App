560de04782d482cf75e8b0ae4dd60478013ccb89
Vivek Choksi <viv.choksi@gmail.com>
2016-03-08 16:14:11 -0800

    Create skeleton for revised prototype; delete all unneeded files

--- CS247App/MilestonesHomeViewController.swift
+++ CS247App/MilestonesHomeViewController.swift
@@ -1,29 +1,24 @@
 //
-//  ViewController.swift
+//  MilestonesHomeViewController.swift
 //  CS247App
 //
-//  Created by Vivek on 2/28/16.
+//  Created by Vivek on 3/9/16.
 //  Copyright © 2016 CS247. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MilestonesHomeViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func toExercise(sender: AnyObject) {
-        performSegueWithIdentifier("toExercise", sender: sender)
-    }
-
-
 }
 
