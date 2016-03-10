cdb236665d6ba5e3ff4868e20cb423ccf73f6e30
Vivek Choksi <viv.choksi@gmail.com>
2016-03-09 20:55:59 -0800

    Finish app flow

---
+++ CS247App/Globals.swift
@@ -0,0 +1,17 @@
+//
+//  Globals.swift
+//  CS247App
+//
+//  Created by Vivek on 3/9/16.
+//  Copyright © 2016 CS247. All rights reserved.
+//
+
+import Foundation
+
+struct Globals {
+    static var hasDoneSquats = false
+    static var hasDoneLegExts = false
+    static var hasGottenMilestone = false
+    static let SQUATS = 1
+    static let LEGEXTS = 2
+}
